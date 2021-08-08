7d4428e8fc4f1f352c09811e53478467536c4160
Taufik Pragusga <pragusga25@gmail.com>
2021-08-08 18:39:09 +0700

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const flag = process.env.FLAG || 'CTFTC{REDACTED}';
+
+app.get('/', (_, res) => {
+  res.send('<h1>Hello You</h1>');
+});
+
+app.get('/flag', (_, res) => {
+  res.send(`This is your flag: ${flag}`);
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
