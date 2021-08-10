688be9085f84fce77f6a56926fecac2ac81b6295
Taufik Pragusga <pragusga25@gmail.com>
2021-08-10 13:33:01 +0700

    Create simple crud app

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const {
   MONGO_IP,
   MONGO_PORT,
 } = require('./config/config');
+const postRouter = require('./routes/postRoutes');
 
 const app = express();
 
@@ -30,6 +31,8 @@ const port = process.env.PORT || 1234;
 
 const flag = process.env.FLAG || 'CTFTC{REDACTED}';
 
+app.use(express.json());
+
 app.get('/', (_, res) => {
   res.send(
     `<h1>Hello Dunia</h1> <p>Listening on port ${port}</p> <p>I'm in ${process.env.NODE_ENV}</p>`
@@ -39,6 +42,8 @@ app.get('/', (_, res) => {
 app.get('/flag', (_, res) => {
   res.send(`This is your flag: ${flag}`);
 });
+
+app.use('/api/posts', postRouter);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
